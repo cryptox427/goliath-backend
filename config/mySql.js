87099d6f8e6e9a875994a5f9cf3350dac23dd52f
dajijel <danijel3726@outlook.com>
2022-08-20 02:04:02 -0400

    update

--- config/mySql.js
+++ config/mySql.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "goliathback.conhgft0hcmt.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "gl1M4yO1Hj1vOhCk",
   database: "opensea"
 });
 
